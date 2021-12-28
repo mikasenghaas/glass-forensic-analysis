Major Improvement on NeuralNetClassifier - Fixed softmax function (only output values in range [0, 1]) - Altered Var Class to have log(), exp() and __float__(), __int__() methods - Made cross entropy function work and set as default for output layer - Made NeuralNetClassifier child of base classifier - Implemented custom hot_encode() function to replace sklearns implementation

src/scripts/models/neural_net/_autograd.py
@@ -1,4 +1,5 @@
-from math import tanh, log10
+from math import tanh, log, log10, exp
+import numpy as np 
 
 class Var:
     """
@@ -50,19 +51,42 @@ class Var:
         return Var(self.v if self.v > 0.0 else 0.0, [(self, 1.0 if self.v > 0.0 else 0.0)])
 
     def log(self):
-        return Var(log10(self.v), self.grad)
+        return Var(log10(self.v), [(self, 1 / self.v * log(10))]) 
+
+    def exp(self):
+        return Var(exp(self.v), [(self, exp(self.v))])
+
+    def __float__(self):
+        return self.v
+
+    def __int__(self):
+        return int(self.v)
 
     def __repr__(self):
-        return f'{self.v}'
-        #return f"Var(v={self.v}, grad={self.grad})"
+        #return f'{self.v}'
+        return f"Var(v={self.v}, grad={self.grad})"
 
 
 if __name__ == '__main__':
     import numpy as np
 
+    p = np.array([[Var(.8), Var(.2)], [Var(.2), Var(.8)]])
+    y = np.array([[Var(1), Var(0)], [Var(0), Var(1)]])
+
+    print(-np.sum(np.log(p) * y))
+
+    def ce(p, y):
+        print(p)
+        print(np.log(p))
+        return -np.sum(np.log(p) * y)
+
+    print(ce(p, y))
+
+    """
     activation = np.vectorize(lambda x:x.relu())
 
     x = np.array([[Var(5), Var(3)], [Var(2), Var(2)]])
     y = np.array([Var(2), Var(2)])
 
     print(activation(x @ y + Var(1)))
+    """

src/scripts/models/neural_net/_helper.py
@@ -10,5 +10,22 @@ def convert_to_var(arr):
 
     return np.array(arr).reshape(shape)
 
-def softmax(y_hot):
-    return (y_hot.T / np.sum(y_hot, axis=1)).T
+def hot_encode(y, intcode):
+    k = len(intcode) 
+    n = len(y)
+
+    y_hot = np.empty((n, k)) 
+
+    for i in range(n):
+        for j in range(k):
+            if j == intcode[y[i]]:
+                y_hot[i, j] = 1
+            else:
+                y_hot[i, j] = 0
+
+    return y_hot.astype(int)
+
+
+def softmax(y):
+    # y is a 2-dimensional matrix nxp
+    return (np.exp(y.T) / np.sum(np.exp(y), axis=1)).T

src/scripts/models/neural_net/_neural_net.py
@@ -3,14 +3,17 @@ from tqdm import tqdm
 from icecream import ic
 from sklearn.preprocessing import OneHotEncoder
 
+from ...base import BaseClassifier
 from ...utils import validate_feature_matrix, validate_target_vector
 from ...metrics import se, mse, cross_entropy 
 from ._autograd import Var
 from ._dense_layer import DenseLayer
-from ._helper import convert_to_var, softmax
+from ._helper import convert_to_var, softmax, hot_encode
 
-class NeuralNetworkClassifier:
-    def __init__(self, layers=[], loss='squared_loss', name='NeuralNetworkClassifier'):
+class NeuralNetworkClassifier(BaseClassifier):
+    def __init__(self, layers=[], loss='squared_error', name='NeuralNetworkClassifier'):
+        super().__init__()
+
         self.name = name
 
         self.X = self.y = self.n = self.p = None
@@ -22,12 +25,12 @@ class NeuralNetworkClassifier:
 
         if loss == 'cross_entropy':
             self.loss = cross_entropy
-        elif loss == 'squared_loss':
+        elif loss == 'squared_error':
             self.loss = se
         elif loss == 'mean_squared_error':
             self.loss = mse
         else:
-            raise NotImplementedError(f"{loss} not yet implemented. Choose from ['cross_entropy', 'squared_loss', 'mean_squared_error']") 
+            raise NotImplementedError(f"{loss} not yet implemented. Choose from ['cross_entropy', 'squared_error', 'mean_squared_error']") 
 
     def add(self, layer):
         self.layers.append(layer)
@@ -44,19 +47,26 @@ class NeuralNetworkClassifier:
     def fit(self, X, y, batch_size=1, epochs=1000, lr = 0.01, verbose=0):
         self.X = validate_feature_matrix(X)
         self.X = convert_to_var(self.X)
+        self.y = y 
 
         self.n, self.p = self.X.shape
         self.k = len(np.unique(y))
         
         # add output layer
-        output_layer = DenseLayer(self.layers[-1].neurons(), self.k, activation='tanh', name='OutputLayer')
+        output_layer = DenseLayer(self.layers[-1].neurons(), self.k, activation='softmax', name='Output')
         self.add(output_layer)
 
-        #self.y = validate_target_vector(y)
-        encoder = OneHotEncoder(sparse=False)
-        y = encoder.fit_transform(y.reshape(-1, 1))
-        self.y = convert_to_var(y)
+        # populate label-intcode dictionaries
+        unique_classes = np.unique(y)
 
+        self.label = {k: unique_classes[k] for k in range(self.k)}
+        self.intcode = {unique_classes[k]:k for k in range(self.k)}
+
+        # one hot encode y (into nxk matrix of Vars)
+        y_hot = hot_encode(self.y, self.intcode)
+        self.y_hot = convert_to_var(y_hot)
+
+        # compute batch size
         batch_size = int(X.shape[0] * batch_size)
 
         # training loop
@@ -65,15 +75,17 @@ class NeuralNetworkClassifier:
             if batch_size < self.n :
                 batch_idxs = np.random.choice(list(range(self.n)), batch_size, replace=False) 
                 X_batch = self.X[batch_idxs]
-                y_batch = self.y[batch_idxs]
+                y_batch = self.y_hot[batch_idxs]
             else: 
                 X_batch = self.X
-                y_batch = self.y
+                y_batch = self.y_hot
 
             # get the probabilities for each datapoint of belonging to each class
             probs = self.forward(X_batch) # n x k matrix of probs for each data point for each class 
 
             # compute loss on one-hot encoded target matrix
+
+            assert not np.any(probs < Var(0.0)), 'probs must be > 0, due to softmax'
             loss = self.loss(y_batch, probs)
 
             # append loss to training history
@@ -126,4 +138,3 @@ class NeuralNetworkClassifier:
         s += '=' * (len('Layer Name\tWeight Dim\tBias Dim\tTotal Parameters\n')+20) + '\n'
         s += f'\t\t\t\t\t\t{self._total_parameters()}'
         return s
-