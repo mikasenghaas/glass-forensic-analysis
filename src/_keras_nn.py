a2d94ef5f1d5c08996c36f98e3ddec04da1583f6
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-12-28 23:17:26 +0100

    Improvements to Custom NN and Keras NN on project data and cleaned up scripts

--- src/_keras_nn.py
+++ src/_keras_nn.py
@@ -1,14 +1,15 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from scripts.utils import get_data
+from scripts.metrics import accuracy_score, confusion_matrix
+from sklearn.preprocessing import OneHotEncoder
 
 from keras.models import Sequential
 from keras.layers import Dense
 from tensorflow.keras.optimizers import Adam, SGD
 
-from sklearn.metrics import accuracy_score
+np.random.seed(1)
 
 def predict(nn, X, label):
     # function to predict from one hot encodeded probs matrix -> labels
@@ -18,76 +19,68 @@ def predict(nn, X, label):
     return preds
 
 def main():
+    # ------ loading and preprocessing ------
+
     # load and split data
-    train = np.loadtxt('./data/raw/df_train.csv', skiprows=1, delimiter=',')
-    test = np.loadtxt('./data/raw/df_test.csv', skiprows=1, delimiter=',')
+    X_train, X_val, X_test, y_train, y_val, y_test = get_data(raw=False, scaled=True)
 
-    X_train, y_train = train[:, :-1], train[:, -1].astype(int)
-    X_test, y_test = test[:, :-1], test[:, -1].astype(int)
-
-    labels = np.unique(y_train)
+    # define mappings from target vector
+    labels = np.unique(np.hstack((y_train, y_test)))
     intcode = {labels[k]:k for k in range(len(labels))}
     label = {k:labels[k] for k in range(len(labels))}
-
-    train_n = len(X_train)
-    test_n = len(X_test)
-
-    # scale feature matrix
-    X = np.vstack((X_train, X_test))
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X)
-
-    X_train, X_test = X[:train_n, :], X[train_n:, :]
 
     # one hot encode target vector 
     encoder = OneHotEncoder(sparse=False)
     y_train_hot = encoder.fit_transform(y_train.reshape(-1, 1))
+    y_val_hot = encoder.fit_transform(y_val.reshape(-1, 1))
     y_test_hot = encoder.fit_transform(y_test.reshape(-1, 1))
 
 
-    #-------------------
-    # construct model
+    #------ constructing model ------
+    
+    # network architecture
     nn = Sequential()
-    nn.add(Dense(30, input_shape=(9,), activation='relu', name='fc1'))
+    nn.add(Dense(20, input_shape=(9,), activation='relu', name='fc1'))
     nn.add(Dense(6, activation='softmax', name='output'))
 
-    # Adam optimizer with learning rate of 0.001
+    # define optimiser, loss and optimisation target
     nn.compile(
             optimizer=SGD(lr=0.1),
             loss='categorical_crossentropy', 
             metrics=['accuracy'])
 
-    print('Neural Network Model Summary: ')
+    # print overview of neural net architecture (number of (active) params to train per layer and in total)
     print(nn.summary())
 
-    # Train the model
-    print('Starting Training')
+    # train model
+    epochs = 100
     history = nn.fit(X_train, y_train_hot,
                batch_size=1,
-               epochs=300,
-               verbose=2)
-               #validation_data=(X_test, y_test_hot))
+               epochs=epochs,
+               verbose=2,
+               validation_data=(X_val, y_val_hot)) # train with validation split (here test data)
 
+    # plot training/ validation accuracy and loss history
+    fig, ax = plt.subplots(ncols=2, figsize=(8,3))
+    for i, title in zip(range(2), ['loss', 'accuracy']):
+        ax[i].plot(list(range(1, epochs+1)), history.history[title], c='blue', label=f'Training {title.title()}')
+        ax[i].plot(list(range(1, epochs+1)), history.history['val_'+title], c='red', label=f'Validation {title.title()}') 
+        ax[i].set_title(f'History of {title.title()}')
+        ax[i].set_xlabel('#Epochs')
+        ax[i].legend(loc='best')
+    plt.show()
 
     train_preds = predict(nn, X_train, label)
     test_preds = predict(nn, X_test, label)
-
-    """
-    print(train_preds, y_train)
-    print(test_preds, y_test)
-
-    print(accuracy_score(y_train, train_preds))
-    print(accuracy_score(y_test, test_preds))
-    """
 
     # pred = np.argmax(nn.predict(x), axis=1)
     train_res = nn.evaluate(X_train, y_train_hot, verbose=0)
     test_res = nn.evaluate(X_test, y_test_hot, verbose=0)
 
 
-    #print(f'Final Train Loss: {train_res[0]}')
-    print(f'\nFinal Train Accuracy: {train_res[1]}')
-    #print(f'Final Test Loss: {test_res[0]}')
+    print(f'\nFinal Train Loss: {train_res[0]}')
+    print(f'Final Train Accuracy: {train_res[1]}\n')
+    print(f'Final Test Loss: {test_res[0]}')
     print(f'Final Test Accuracy: {test_res[1]}\n')
 
 if __name__ == '__main__':
