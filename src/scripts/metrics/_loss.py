cdced7e7b8e314d48b29797c83e3f423baed738d
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-12-29 14:03:51 +0100

    Added Testcases

--- src/scripts/metrics/_loss.py
+++ src/scripts/metrics/_loss.py
@@ -18,12 +18,14 @@ def binary_cross_entropy(y, p):
     return - (1 / len(y)) * np.sum(y * np.log(p) + ((1+y) * np.log(1-p)))
 
 def cross_entropy(y, p):
-    #return - (1 / y.shape[0]) * np.sum(y * np.log(p))
-    """
-    if y.ndim == 1:
-        y = y.reshape(-1, 1)
-    if p.ndim == 1:
-        p = p.reshape(-1, 1)
-    """
-
     return -np.sum(np.log(p) * y)
+
+if __name__ == '__main__':
+    p = np.array([[0.25,0.25,0.25,0.25],
+                  [0.01,0.01,0.01,0.96]])
+    y = np.array([[0,0,0,1],
+                  [0,0,0,1]])
+
+    ans = 0.71355817782  #Correct answer
+    x = cross_entropy(y, p)
+    print(np.isclose(x,ans))
