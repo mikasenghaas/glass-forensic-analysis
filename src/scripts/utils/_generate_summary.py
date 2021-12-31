fbf2c063554b4f6b6ae576aca81090cfc2dce763
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2021-12-31 22:56:36 +0100

    Rewrote generate_summary() function to be callable from individual script Easier for saving training results while working on a model

--- src/scripts/utils/_generate_summary.py
+++ src/scripts/utils/_generate_summary.py
@@ -2,7 +2,9 @@ import sys
 from config.definitions import ROOT_DIR
 import os
 from tqdm import tqdm
+from datetime import datetime
 
+"""
 from models.custom_dt import main as main1
 from models.custom_nn import main as main2
 from models.sklearn_dt import main as main3
@@ -32,3 +34,20 @@ def generate_summary():
         sys.stdout = open(out_path, 'w')
         SUMMARIES[name]()
         sys.stdout.close()
+"""
+
+def generate_summary(**kwargs):
+    for key, val in kwargs.items():
+        if key == 'filepath': 
+            filepath = val;
+        elif key == 'name': 
+            name = val
+            s = f"Model: {name.replace('_', ' ').title()}\n"
+            s += f"Training Time: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n"
+            s += "-"*10 + '\n\n'
+        else: s += f"{key.__str__().replace('_', ' ').title()}\n{'-'*len(key.__str__())}\n{val.__str__()}\n\n"
+    
+    with open(f'{filepath}/{name}_results.txt', 'w') as outfile:
+        outfile.write(s)
+
+    print(f'Saved model training results at {filepath}/{name}_results.txt')
