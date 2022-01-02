76a71c526267352c9375211eba967072de04df29
Jonas-Mika Senghaas <accounts@jonassenghaas.de>
2022-01-02 19:47:51 +0100

    slight modification to eda

--- src/eda/_feature_inspection.py
+++ src/eda/_feature_inspection.py
@@ -14,16 +14,54 @@ import sys
 import os
 
 # Global constants
+X_train, X_test, y_train, y_test = get_data(raw=True, scaled=False)
 features_names = ['refractive_index', 'sodium', 'magnesium', 'aluminium', 'silicone', 'potassium', 'calcium', 'barium', 'iron']
 
 def run_eda():
-
+    _class_distribution()
+    _feature_five_num_summaries()
     _each_class_distribution_per_feature()
     _best_2_pca_comp_scatter()
 
+def _class_distribution():
+    train = Counter(y_train) 
+    test = Counter(y_test)
+    total = train + test
+
+    def normalise(d):
+        s = sum(d.values())
+        for key in d:
+            d[key] /= s
+            d[key] *= 100
+        return d 
+
+    train = normalise(train)
+    test = normalise(test)
+
+    fig, ax = plt.subplots(ncols=2, figsize=(4*2, 3))
+
+    for i, info in enumerate(zip([train, test], ['train_split', 'test_split'])):
+        count, name = info
+        sns.barplot(x=list(count.keys()), y=list(count.values()), palette="Blues_d", ax = ax[i])
+        ax[i].set_title(f"{name.replace('_', ' ').title()} Class Distribution")
+        ax[i].set_xlabel('Glass Types')
+        if i == 0:
+            ax[i].set_ylabel('Relative Frequency (%)')
+
+    fig.savefig('./data/figures/class_distribution.pdf')
+    print('Saved class distribution to ./data/figures/class_distribution.pdf')
+
+def _feature_fivenumsummaries():
+    data = np.vstack((X_train, X_test))
+
+    summaries = np.percentile(data, [0, 25, 50, 75, 100], axis=0).T
+
+    df = pd.DataFrame(summaries, columns = ['Min','Lower Quartile','Median', 'Upper Quartile', 'Max'], index=['RI', 'Na', 'Mg', 'Al', 'Si', 'K', 'Ca', 'Ba', 'Fe'])
+
+    print(df)
+
 
 def _each_class_distribution_per_feature():
-
     train_scaled = pd.read_csv(os.path.join(ROOT_DIR, 'data', 'transformed', 'train', 'X_scaled.csv'), delimiter=',', names=features_names)
     y = pd.read_csv(os.path.join(ROOT_DIR, 'data', 'transformed', 'train', 'y.csv'), delimiter=',', header=None)
 
@@ -65,7 +103,6 @@ def _each_class_distribution_per_feature():
     fig.savefig(os.path.join(ROOT_DIR, 'data', 'figures', 'fdist_violin.jpg'))
 
 def _best_2_pca_comp_scatter():
-
     train_pca = pd.read_csv(os.path.join(ROOT_DIR, 'data', 'transformed', 'train', 'X_pca.csv'), delimiter=',', header=None)
     y = pd.read_csv(os.path.join(ROOT_DIR, 'data', 'transformed', 'train', 'y.csv'), delimiter=',', header=None)
 
